0620107299927ba09e9b9e5843474354ba1c743d
Soumya Ray <soumya.ray@gmail.com>
2016-04-08 07:28:42 +0800

    specs/api_spec: update expectation notation

--- specs/api_spec.rb
+++ specs/api_spec.rb
@@ -3,8 +3,8 @@ require_relative './spec_helper'
 describe 'Test root route' do
   it 'should find the root route' do
     get '/'
-    last_response.body.must_include 'ConfigShare'
-    last_response.status.must_equal 200
+    _(last_response.body).must_include 'ConfigShare'
+    _(last_response.status).must_equal 200
   end
 end
 
